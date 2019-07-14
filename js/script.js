17c6bef9ddeabb3456010e125c799beceb33bf24
Serhii Khomenko <cahxoce2101@gmail.com>
2019-07-14 15:07:43 +0300

    addStickyNav+bugFix

--- js/script.js
+++ js/script.js
@@ -1,13 +1,27 @@
-$(function() {
-    var owl = $(".owl-carousel");
-    owl.owlCarousel({
-      items: 1,
-      loop: true,
-      nav: true,
-      autoplay: true,
-      autoplayTimeout: 3000,
-      smartSpeed: 4000,
-      animateOut: "slideOutRight",
-      animateIn: "slideInLeft",
-    });
+$(function () {
+  var owl = $(".owl-carousel");
+  owl.owlCarousel({
+    items: 1,
+    loop: true,
+    nav: true,
+    autoplay: true,
+    autoplayTimeout: 3000,
+    smartSpeed: 4000,
+    animateOut: "slideOutRight",
+    animateIn: "slideInLeft",
   });
+});
+
+
+
+// nav sticky
+var navbar = document.querySelector('.navbar');
+window.addEventListener('scroll', function () {
+
+  var scrollX = this.scrollY;
+  if (scrollX > 100) {
+    navbar.classList.add('sticky');
+  } else {
+    navbar.classList.remove('sticky');
+  }
+});
